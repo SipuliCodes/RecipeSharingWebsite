133a04c5b6470f35d3672f6fd9efc922c77e8c6c
SipuliCodes <vepsalainensisu@gmail.com>
2024-05-21 19:10:23 +0300

    added password encryption

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,8 +1,21 @@
+import bcrypt from 'bcrypt';
+
 import { IUser, NewUser } from '../interfaces/userInterfaces';
 import User from '../models/user';
 
 const addUser = async (user: NewUser): Promise<IUser> => {
-  const addedUser = new User(user);
+  const { firstName, lastName, username, email, password } = user;
+
+  const saltRounds = 10;
+  const passwordHash: string = await bcrypt.hash(password, saltRounds);
+
+  const addedUser = new User({
+    firstName,
+    lastName,
+    username,
+    email,
+    password: passwordHash,
+  });
   await addedUser.save();
 
   return addedUser;
