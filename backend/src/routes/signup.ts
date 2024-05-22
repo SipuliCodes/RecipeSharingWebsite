e88f87bda1a4fec20e4edad33124c59e82ac27cb
SipuliCodes <vepsalainensisu@gmail.com>
2024-05-22 15:46:23 +0300

    made changes to signup errorhandling

--- backend/src/routes/signup.ts
+++ backend/src/routes/signup.ts
@@ -1,30 +1,41 @@
 import { Router } from "express";
+import { MongoServerError } from "mongodb";
 
 import userService from "../services/userService";
 import { IUser } from "../interfaces/userInterfaces";
-import { toNewUser } from '../utils/requestValidations';
+import { toNewUser } from "../utils/requestValidations";
 import { createToken } from "../utils/token";
 
 const router: Router = Router();
 
-router.get('/', (_req, res) => {
-  res.send('signup');
+router.get("/", (_req, res) => {
+  res.send("signup");
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   try {
     const newUser = toNewUser(req.body);
 
-    userService.addUser(newUser).then((addedUser: IUser) => {
-      const token = createToken(addedUser.username, addedUser.id);
-      res.json({addedUser, token});
-  }).catch((error) => {
-    console.log(error);
-  });
+    userService
+      .addUser(newUser)
+      .then((addedUser: IUser) => {
+        const token = createToken(addedUser.username, addedUser.id);
+        res.json({ addedUser, token });
+      })
+      .catch((error: unknown) => {
+        let errorMessage = "Something went wrong.";
+        if (error instanceof MongoServerError) {
+          res.status(400).json(error.keyValue);
+        }
+        if (error instanceof Error) {
+          errorMessage += "Error: " + error.message;
+        }
+        res.status(400).json(errorMessage);
+      });
   } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
+    let errorMessage = "Something went wrong.";
     if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
+      errorMessage += " Error: " + error.message;
     }
     res.status(400).send(errorMessage);
   }
