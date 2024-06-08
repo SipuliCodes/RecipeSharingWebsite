4e9f0fe1021227e8436a92f4a8614d7af682f160
SipuliCodes <vepsalainensisu@gmail.com>
2024-06-08 12:28:04 +0300

    added logout button to sidebar

--- frontend/src/components/HomePage/Sidebar/Sidebar.tsx
+++ frontend/src/components/HomePage/Sidebar/Sidebar.tsx
@@ -1,11 +1,20 @@
-import './Sidebar.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   findIconDefinition
 } from '@fortawesome/fontawesome-svg-core';
+import { useContext } from 'react';
+
+import './Sidebar.css';
+import { removeToken } from '../../../utils/localStorage';
+import { UserSetTokenContext } from '../../../contexts/userContext';
 
 const Sidebar = () => {
+  const setTokenContext = useContext(UserSetTokenContext);
+
+  const logout = () => {
+    removeToken();
+    setTokenContext('');
+  };
 
   return (
     <div className='sidebar'>
@@ -17,6 +26,7 @@ const Sidebar = () => {
         <h3 className='sidebar-links'>Friends</h3>
         <h3 className='sidebar-links'>Settings</h3>
       </div>
+      <button onClick={logout} className='logout-button'>Log out</button>
     </div>
   );
 };
