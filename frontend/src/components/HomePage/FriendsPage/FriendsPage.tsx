f4e172f74e26b270b05e5eba1320d401f95c432d
SipuliCodes <vepsalainensisu@gmail.com>
2024-07-04 17:34:15 +0300

    changed friend, request listst to use real data instead of fake

--- frontend/src/components/HomePage/FriendsPage/FriendsPage.tsx
+++ frontend/src/components/HomePage/FriendsPage/FriendsPage.tsx
@@ -3,7 +3,6 @@ import { useContext } from 'react';
 import useAutoNavigation from '../../../hooks/useAutoNavigation';
 import { UserDetailsContext, UserTokenContext } from '../../../contexts/userContext';
 import './FriendsPage.css';
-import { Friend } from '../../../interfaces/userInterfaces';
 import FriendCard from './FriendCard/FriendCard';
 
 const FriendsPage = () => {
@@ -14,96 +13,12 @@ const FriendsPage = () => {
   
   useAutoNavigation('/', token, false);
 
-  const friendFriends: Friend[] = [
-    {
-      username: 'Zorro',
-      firstName: 'John',
-      lastName: 'Melton',
-      id: '1'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '2'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '6'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '7'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '8'
-    }
-  ];
-
-  const friends: Friend[] = [
-    {
-      username: 'Zorro',
-      firstName: 'John',
-      lastName: 'Melton',
-      id: '1'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '2'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '3'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '4'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '5'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '6'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '7'
-    },
-    {
-      username: 'Batman',
-      firstName: 'Greg',
-      lastName: 'Maloni',
-      id: '8'
-    }
-  ];
-
   return (
     <div className='friends-page'>
       <div className='friends-page-friends-box'>
         <h1 className='friends-page-h1'>Friends</h1>
         <div className='friends-page-friends-list'>
-          {friends.map(friend => <FriendCard key={friend.id} friend={friend} requestType='none' />)}
+          {user.friends.map(friend => <FriendCard key={friend.id} friend={friend} requestType='none' />)}
         </div>
       </div>
       <div className='friends-page-friend-requests-box'>
@@ -111,12 +26,12 @@ const FriendsPage = () => {
         <div className='friends-page-friend-requests-divider'>
           <div className='friends-page-friends-request-list'>
             <h2 className='friends-page-h2'>Received</h2>
-            {friendFriends.map(friend => <FriendCard key={friend.id} friend={friend} requestType='incoming' />)}
+            {user.receivedRequests?.map(friend => <FriendCard key={friend.id} friend={friend} requestType='incoming' />)}
           </div>
           <div></div>
           <div className='friends-page-friends-request-list'>
             <h2 className='friends-page-h2'>Sent</h2>
-            {friendFriends.map(friend => <FriendCard key={friend.id} friend={friend} requestType='pending' />)}
+            {user.sentRequests?.map(friend => <FriendCard key={friend.id} friend={friend} requestType='pending' />)}
           </div>
         </div>
       </div>
