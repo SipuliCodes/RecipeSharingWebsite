a4cd515f26761debc3dbe71d6746857bbd5494da
SipuliCodes <vepsalainensisu@gmail.com>
2024-07-07 20:58:07 +0300

    design for settings page done

--- frontend/src/components/SettingsPage/SettingsPage.tsx
+++ frontend/src/components/SettingsPage/SettingsPage.tsx
@@ -1,14 +1,41 @@
 import { useContext } from 'react';
 
+import './SettingsPage.css';
 import { UserDetailsContext } from '../../contexts/userContext';
 
 const SettingsPage = () => {
   const user = useContext(UserDetailsContext);
 
   return (
-    <div>
-      <h1>Settings</h1>
-      <h2>{user.username}</h2>
+    <div className='settings-page-container'>
+      <div className='settings-page-form'>
+        <h1 className='settings-page-h1'>Settings</h1>
+        <div className='settings-page-user-details-box'>
+          <div className='settings-page-user-name-box'>
+            <h3 className='settings-page-h3'> Change user details</h3>
+            <div className='settings-page-current-and-input'>
+              <h5 className='settings-page-h5'>Current first name: {user.firstName}</h5>
+              <input className='settings-page-input' type='text' placeholder='New first name'></input>
+            </div>
+            <div className='settings-page-current-and-input'>
+              <h5 className='settings-page-h5'>Current last name: {user.lastName}</h5>
+              <input className='settings-page-input' type='text' placeholder='New last name'></input>
+            </div>
+            <div className='settings-page-current-and-input'>
+              <h5 className='settings-page-h5'>Current email: {user.email}</h5>
+              <input className='settings-page-input' type='text' placeholder='New first name'></input>
+            </div>
+            <button className='settings-page-button'>Confirm changes</button>
+          </div>
+          <div className='settings-page-password-box'>
+            <h3 className='settings-page-h3'> Change password</h3>
+            <input className='settings-page-input settings-page-password-input' type='password' placeholder='Current password'></input>
+            <input className='settings-page-input settings-page-password-input' type='password' placeholder='New password'></input>
+            <input className='settings-page-input settings-page-password-input' type='password' placeholder='New password again'></input>
+            <button className='settings-page-button'>Confirm new password</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
