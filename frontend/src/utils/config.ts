fdb91103ef063fbf949062ec20605a11335b96e1
SipuliCodes <vepsalainensisu@gmail.com>
2024-08-17 19:46:01 +0300

    added different url for production

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,3 +1,8 @@
+const url =
+  process.env.NODE_ENV === 'production'
+    ? 'https://recipesharingwebsite.onrender.com/api'
+    : 'http://localhost:3003/api'; 
+
 export const config = {
-  apiUrl: 'http://localhost:3003/api'
+  apiUrl: url
 };
